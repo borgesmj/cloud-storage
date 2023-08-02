3f900e4faf0280dd13bc3ee9d710493f29fbec69
Miguel José <121818423+borgesmj@users.noreply.github.com>
2023-08-02 10:58:11 -0500

    Create Enlaces.js

---
+++ Enlaces.js
@@ -0,0 +1,6 @@
+export default const enlaces = [
+  text: 'Envío de Remesas', url: 'https://borgesmj.github.io/remesas'}.
+  text: 'Tienda Virtual', url: '#'}.
+  text: 'Cursos Digitales', url: '#'}.
+  text: 'Blog', url: '#'}.
+]
